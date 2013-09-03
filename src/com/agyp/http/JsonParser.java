89e0c3137011f5b45fb08e39aab2a8387b1bf885
ajay <ajay@ajay-Inspiron-1545.(none)>
2013-09-03 10:41:35 +0530

    parser class added

---
+++ src/com/agyp/http/JsonParser.java
@@ -0,0 +1,25 @@
+package com.agyp.http;
+
+import java.io.InputStream;
+
+/**
+ * This class is used to parse Json response.
+ * 
+ * @author ajay
+ * 
+ */
+public class JsonParser {
+	private final static String TAG = JsonParser.class.getSimpleName();
+
+	public JsonParser() {
+	}
+
+	public void parseData(final InputStream inputStream) {
+
+	}
+
+	public void getDataFromResponse(final InputStream inputStream) {
+
+	}
+
+}
