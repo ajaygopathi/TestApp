536f7a74cb015701c201ed7ec4fd7216e738a0ca
ajay <ajay@ajay-Inspiron-1545.(none)>
2013-09-03 16:01:10 +0530

    network handling stuff added

--- src/com/agyp/http/HttpRequestConstants.java
+++ src/com/agyp/http/HttpRequestConstants.java
@@ -1,6 +1,5 @@
 package com.agyp.http;
 
-import com.agyp.utils.TestAppConstants;
 
 /**
  * This class is used to define web service request ids and request urls
@@ -12,20 +11,10 @@ public final class HttpRequestConstants {
 	private final static String TAG = HttpRequestConstants.class
 			.getSimpleName();
 
-	private final static String AUTHURL = "https://api.instagram.com/oauth/authorize/";
-	private static final String TOKENURL = "https://api.instagram.com/oauth/access_token";
-	private static final String APIURL = "https://api.instagram.com/v1";
+	public final static String AUTHURL = "https://api.instagram.com/oauth/authorize/";
+	public static final String TOKENURL = "https://api.instagram.com/oauth/access_token";
+	public static final String APIURL = "https://api.instagram.com/v1";
 
-	public static final String STRING_AUTHURL = AUTHURL
-			+ "?client_id="
-			+ TestAppConstants.CLIENT_ID
-			+ "&redirect_uri="
-			+ TestAppConstants.CALLBACKURL
-			+ "&response_type=code&display=touch&scope=likes+comments+relationships";
-
-	public static final String STRING_TOKENURL = TOKENURL + "?client_id="
-			+ TestAppConstants.CLIENT_ID + "&client_secret="
-			+ TestAppConstants.CLIENT_SECRET + "&redirect_uri="
-			+ TestAppConstants.CALLBACKURL + "&grant_type=authorization_code";
+	
 
 }
--- src/com/agyp/testapp/Authentication.java
+++ src/com/agyp/testapp/Authentication.java
@@ -1,11 +1,25 @@
 package com.agyp.testapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.agyp.http.HttpRequestConstants;
 import com.agyp.testapp.LoginDialog.AuthDialogListener;
+import com.agyp.utils.TestAppConstants;
 
 public class Authentication {
 
@@ -14,22 +28,35 @@ public class Authentication {
 
 	private String mAccessTokenString;
 	private String mToken;
-
 	private AuthAuthenticationListener mAuthAuthenticationListener;
-
 	private ProgressDialog mProgressDialog;
 	private Context mContext;
+	private String mAuthUrl;
+	private String mTokenUrl;
 
 	public Authentication(Context context) {
 		mContext = context;
 		// mSessionStore = new SessionStore(context);
 
+		mAuthUrl = HttpRequestConstants.AUTHURL
+				+ "?client_id="
+				+ TestAppConstants.CLIENT_ID
+				+ "&redirect_uri="
+				+ TestAppConstants.CALLBACKURL
+				+ "&response_type=code&display=touch&scope=likes+comments+relationships";
+
+		mTokenUrl = HttpRequestConstants.TOKENURL + "?client_id="
+				+ TestAppConstants.CLIENT_ID + "&client_secret="
+				+ TestAppConstants.CLIENT_SECRET + "&redirect_uri="
+				+ TestAppConstants.CALLBACKURL
+				+ "&grant_type=authorization_code";
+
 		ClientAuthDialogListener instaAuthDialogListener = new ClientAuthDialogListener();
 
 		// mAccessTokenString = mSessionStore.getInstaAccessToken();
 
-		LoginDialog instaLoginDialog = new LoginDialog(context,
-				HttpRequestConstants.STRING_AUTHURL, instaAuthDialogListener);
+		LoginDialog instaLoginDialog = new LoginDialog(context, mAuthUrl,
+				instaAuthDialogListener);
 
 		instaLoginDialog.show();
 		mProgressDialog = new ProgressDialog(context);
@@ -69,128 +96,125 @@ public class Authentication {
 		// new GetInstagramTokenAsyncTask().execute();
 	}
 
-	// public class GetInstagramTokenAsyncTask extends AsyncTask<Void, Void,
-	// Void> {
-	// @Override
-	// protected Void doInBackground(Void... params) {
-	// try {
-	// URL url = new URL(HttpRequestConstants.STRING_AUTHURL);
-	// HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url
-	// .openConnection();
-	// httpsURLConnection.setRequestMethod("POST");
-	// httpsURLConnection.setDoInput(true);
-	// httpsURLConnection.setDoOutput(true);
-	//
-	// OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
-	// httpsURLConnection.getOutputStream());
-	// outputStreamWriter.write("client_id="
-	// + TestAppConstants.CLIENT_ID + "&client_secret="
-	// + TestAppConstants.CLIENT_SECRET
-	// + "&grant_type=authorization_code" + "&redirect_uri="
-	// + TestAppConstants.CALLBACKURL + "&code=" + mToken);
-	//
-	// outputStreamWriter.flush();
-	//
-	// // Response would be a JSON response sent by instagram
-	// String response = streamToString(httpsURLConnection
-	// .getInputStream());
-	// // Log.e("USER Response", response);
-	// JSONObject jsonObject = (JSONObject) new JSONTokener(response)
-	// .nextValue();
-	//
-	// // Your access token that you can use to make future request
-	// mAccessTokenString = jsonObject.getString("access_token");
-	// // Log.e(TAG, mAccessTokenString);
-	//
-	// // User details like, name, id, tagline, profile pic etc.
-	// JSONObject userJsonObject = jsonObject.getJSONObject("user");
-	// // Log.e("USER DETAIL", userJsonObject.toString());
-	//
-	// // User ID
-	// String id = userJsonObject.getString("id");
-	// // Log.e(TAG, id);
-	//
-	// // Username
-	// String username = userJsonObject.getString("username");
-	// // Log.e(TAG, username);
-	//
-	// // User full name
-	// String name = userJsonObject.getString("full_name");
-	// // Log.e(TAG, name);
-	//
-	// // //TO?DO
-	// // mSessionStore.saveInstagramSession(mAccessTokenString, id,
-	// // username, name);
-	// showResponseDialog(name, mAccessTokenString);
-	// } catch (Exception e) {
-	// mAuthAuthenticationListener
-	// .onFail("Failed to get access token");
-	// e.printStackTrace();
-	// }
-	// return null;
-	// }
-	//
-	// @Override
-	// protected void onPostExecute(Void result) {
-	// dismissDialog();
-	// mAuthAuthenticationListener.onSuccess();
-	// super.onPostExecute(result);
-	// }
-	//
-	// @Override
-	// protected void onPreExecute() {
-	// showDialog("Getting Access Token..");
-	// super.onPreExecute();
-	// }
-	//
-	// }
-	//
+	public class GetInstagramTokenAsyncTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				URL url = new URL(mTokenUrl);
+				HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url
+						.openConnection();
+				httpsURLConnection.setRequestMethod("POST");
+				httpsURLConnection.setDoInput(true);
+				httpsURLConnection.setDoOutput(true);
 
-	//
-	// public String streamToString(InputStream is) throws IOException {
-	// String string = "";
-	//
-	// if (is != null) {
-	// StringBuilder stringBuilder = new StringBuilder();
-	// String line;
-	//
-	// try {
-	// BufferedReader reader = new BufferedReader(
-	// new InputStreamReader(is));
-	//
-	// while ((line = reader.readLine()) != null) {
-	// stringBuilder.append(line);
-	// }
-	//
-	// reader.close();
-	// } finally {
-	// is.close();
-	// }
-	//
-	// string = stringBuilder.toString();
-	// }
-	//
-	// return string;
-	// }
-	//
-	// public void showDialog(String message) {
-	// mProgressDialog.setMessage(message);
-	// mProgressDialog.show();
-	// }
-	//
-	// public void dismissDialog() {
-	// if (mProgressDialog.isShowing()) {
-	// mProgressDialog.dismiss();
-	// }
-	// }
-	//
-	// public void showResponseDialog(String name, String accessToken) {
-	// // Intent broadcastIntent = new Intent();
-	// // broadcastIntent.setAction(ResponseListener.ACTION_RESPONSE);
-	// // broadcastIntent.putExtra(ResponseListener.EXTRA_NAME, name);
-	// // broadcastIntent.putExtra(ResponseListener.EXTRA_ACCESS_TOKEN,
-	// // accessToken);
-	// // mContext.sendBroadcast(broadcastIntent);
-	// }
+				OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+						httpsURLConnection.getOutputStream());
+				outputStreamWriter.write("client_id="
+						+ TestAppConstants.CLIENT_ID + "&client_secret="
+						+ TestAppConstants.CLIENT_SECRET
+						+ "&grant_type=authorization_code" + "&redirect_uri="
+						+ TestAppConstants.CALLBACKURL + "&code=" + mToken);
+
+				outputStreamWriter.flush();
+
+				// Response would be a JSON response sent by instagram
+				String response = streamToString(httpsURLConnection
+						.getInputStream());
+				// Log.e("USER Response", response);
+				JSONObject jsonObject = (JSONObject) new JSONTokener(response)
+						.nextValue();
+
+				// Your access token that you can use to make future request
+				mAccessTokenString = jsonObject.getString("access_token");
+				// Log.e(TAG, mAccessTokenString);
+
+				// User details like, name, id, tagline, profile pic etc.
+				JSONObject userJsonObject = jsonObject.getJSONObject("user");
+				// Log.e("USER DETAIL", userJsonObject.toString());
+
+				// User ID
+				String id = userJsonObject.getString("id");
+				// Log.e(TAG, id);
+
+				// Username
+				String username = userJsonObject.getString("username");
+				// Log.e(TAG, username);
+
+				// User full name
+				String name = userJsonObject.getString("full_name");
+				// Log.e(TAG, name);
+
+				// //TO?DO
+				// mSessionStore.saveInstagramSession(mAccessTokenString, id,
+				// username, name);
+				showResponseDialog(name, mAccessTokenString);
+			} catch (Exception e) {
+				mAuthAuthenticationListener
+						.onFail("Failed to get access token");
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			dismissDialog();
+			mAuthAuthenticationListener.onSuccess();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			showDialog("Getting Access Token..");
+			super.onPreExecute();
+		}
+
+	}
+
+	public String streamToString(InputStream is) throws IOException {
+		String string = "";
+
+		if (is != null) {
+			StringBuilder stringBuilder = new StringBuilder();
+			String line;
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is));
+
+				while ((line = reader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+
+				reader.close();
+			} finally {
+				is.close();
+			}
+
+			string = stringBuilder.toString();
+		}
+
+		return string;
+	}
+
+	public void showDialog(String message) {
+		mProgressDialog.setMessage(message);
+		mProgressDialog.show();
+	}
+
+	public void dismissDialog() {
+		if (mProgressDialog.isShowing()) {
+			mProgressDialog.dismiss();
+		}
+	}
+
+	public void showResponseDialog(String name, String accessToken) {
+		// Intent broadcastIntent = new Intent();
+		// broadcastIntent.setAction(ResponseListener.ACTION_RESPONSE);
+		// broadcastIntent.putExtra(ResponseListener.EXTRA_NAME, name);
+		// broadcastIntent.putExtra(ResponseListener.EXTRA_ACCESS_TOKEN,
+		// accessToken);
+		// mContext.sendBroadcast(broadcastIntent);
+	}
 
 }
